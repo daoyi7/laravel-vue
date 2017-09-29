5131b61d25253bb3cc3c8a4c63aa006c3d959c8b
daoyi7 <kawhichina@gmail.com>
2017-09-29 21:42:36 +0800

    update

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -66,8 +66,11 @@ const routes = [{
 const router = new VueRouter({
     // mode: 'history',
     routes,
-    scrollBehavior (to, from, savedPosition) {
-        return { x: 0, y: 0}
+    scrollBehavior(to, from, savedPosition) {
+        return {
+            x: 0,
+            y: 0
+        }
     }
 })
 
