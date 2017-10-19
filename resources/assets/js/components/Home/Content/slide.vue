54b38141feeab35b99ebaea99b6ab1ddc8eb8c64
daoyi7 <kawhichina@gmail.com>
2017-10-19 14:56:29 +0800

    modified slide.vue

--- resources/assets/js/components/Home/Content/slide.vue
+++ resources/assets/js/components/Home/Content/slide.vue
@@ -1,46 +1,45 @@
 <template>
-    <swiper class="slide-wrap" :options="swiperOption">
-        <swiper-slide class="slide-item" v-for="(slide,index) in swiperSlides" key="index">
-            <router-link class="slide-link" to="/">
-                <span class="slide-title">{{ slide.title }}</span>
-                <img class="slide-thumb" :src="slide.thumb">
-            </router-link>
-        </swiper-slide>
-        <div class="swiper-pagination" slot="pagination"></div>
-    </swiper>
+<swiper class="slide-wrap" :options="swiperOption">
+  <swiper-slide class="slide-item" v-for="(slide,index) in swiperSlides" key="index">
+    <router-link class="slide-link" to="/">
+      <span class="slide-title">{{ slide.title }}</span>
+      <img class="slide-thumb" :src="slide.thumb">
+    </router-link>
+  </swiper-slide>
+  <div class="swiper-pagination" slot="pagination"></div>
+</swiper>
 </template>
 
 <script type="text/ecmascript-6">
-    export default {
-        name: 'carrousel',
-        data() {
-            return {
-                swiperOption: {
-                    autoplay: 3500,
-                    setWrapperSize: true,
-                    pagination: '.swiper-pagination',
-                    paginationClickable: true,
-                    mousewheelControl: false,
-                    observeParents: true,
-                    autoplayDisableOnInteraction: false,
-                },
-                swiperSlides: [
-                    {
-                        thumb:"../../../images/bg.png",
-                        title:"bg1",
-                    },
-                    {
-                        thumb:"../../../images/bg1.png",
-                        title:"bg2",
-                    },
-                    {
-                        thumb:"../../../images/bg2.png",
-                        title:"bg3",
-                    },
-                ]
-            }
+export default {
+  name: 'carrousel',
+  data() {
+    return {
+      swiperOption: {
+        autoplay: 3500,
+        setWrapperSize: true,
+        pagination: '.swiper-pagination',
+        paginationClickable: true,
+        mousewheelControl: false,
+        observeParents: true,
+        autoplayDisableOnInteraction: false,
+      },
+      swiperSlides: [{
+          thumb: "../../../images/bg.png",
+          title: "bg1",
         },
+        {
+          thumb: "../../../images/bg1.png",
+          title: "bg2",
+        },
+        {
+          thumb: "../../../images/bg2.png",
+          title: "bg3",
+        },
+      ]
     }
+  },
+}
 </script>
 
 <style type="text/sass" lang="sass" rel="stylesheet/sass" scoped>
